f42a19810042db315c2eeb72eb33e48af3be7633
Fernando Vaquero <fernando.vaquero@a.vedrunasevillasj.es>
2025-04-18 11:16:50 +0200

    Primer commit - Inicio proyecto

---
+++ src/main/java/com/vedruna/transporte/CoDrive/CoDriveApplication.java
@@ -0,0 +1,13 @@
+package com.vedruna.transporte.CoDrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoDriveApplication.class, args);
+	}
+
+}
